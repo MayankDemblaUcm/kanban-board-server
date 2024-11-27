4eea0290531867db5bbbaea0078e0f22d21ea2fc
Mayankdembla0407 <105827066+MayankDembla0407@users.noreply.github.com>
2024-11-26 21:29:19 -0600

    Adding Boiler Plate Code

---
+++ src/test/java/com/kanbanboard/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanbanboard.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
